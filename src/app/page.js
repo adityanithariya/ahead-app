ab167f9df8527f00310f7551d9287448cf4e4019
Aditya Soni <adityanithariya@gmail.com>
2023-05-31 21:52:29 +0530

    UI Components Separated

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,15 @@
 import NavBar from "@components/NavBar";
 import Hero from "@components/Hero";
-import HeroBottom from "@components/HeroBottom";
 import Emotions from "@components/Emotions";
 import Manifesto from "@components/Manifesto";
-import ManifestoBottom from "@components/ManifestoBottom";
 
 export default function Home() {
     return (
         <>
             <NavBar />
             <Hero />
-            <HeroBottom />
             <Emotions />
             <Manifesto />
-            <ManifestoBottom />
         </>
     );
 }
